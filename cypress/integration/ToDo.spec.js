68b3b2eab960c1ea15cf07ba7a91e2adaffe7b1a
unknown <hamdibc0@gmail.com>
2021-07-23 19:07:38 +0200

    edit spec.js

--- cypress/integration/ToDo.spec.js
+++ cypress/integration/ToDo.spec.js
@@ -1,60 +1,58 @@
 const TodoPage = require("../PageObjects/TodoPage")
+const Login = require("../PageObjects/Login")
 const Signup = require("../PageObjects/Signup")
 const Functions = require("../support/Functions")
 const TestData = require("../support/TestData")
-const Login = require("../PageObjects/Login")
 
 
 context('Validate Todo.ly', () => {
-    beforeEach(() => {
-      cy.visit('http://todo.ly/')
-    })
+  beforeEach(() => {
+    cy.visit('http://todo.ly/')
+  })
 
-it('Verify creating User with invalid email', () => {
-Functions.SignUp(TestData.Invalidemail, TestData.Name, TestData.Pwd)
-cy.get(Signup.Error).should("be.visible").should('have.text',TestData.ErrorMessage)
-})    
+  it('Verify creating User with invalid email', () => {
+    Functions.SignUp(TestData.Invalidemail, TestData.Name, TestData.Pwd)
+    cy.get(Signup.Error).should("be.visible").should('have.text',TestData.ErrorMessage)
+  })    
 
-it('Verify creating user', () => {
-Functions.SignUp(TestData.Validemail, TestData.Name, TestData.Pwd)
-cy.get(TodoPage.ProjectTitle).should("be.visible").should("have.text",TestData.DefaultPrj)
-})    
+  it('Verify creating user', () => {
+    Functions.SignUp(TestData.Validemail, TestData.Name, TestData.Pwd)
+    cy.get(TodoPage.ProjectTitle).should("be.visible").should("have.text",TestData.DefaultPrj)
+  })    
 
-it('Verify Login Negtive', () => {
+  it('Verify Login Negtive', () => {
+    Functions.LogIn(TestData.Invalidemail, TestData.Pwd)
+    cy.get(Login.Error).should("be.visible").should('have.text',TestData.ErrorMessagelog)
+  })
 
-Functions.LogIn(TestData.Invalidemail, TestData.Pwd)
-cy.get(Login.Error).should("be.visible").should('have.text',TestData.ErrorMessagelog)
-})
+  it('Verify Login', () => {
+    Functions.LogIn(TestData.Validemail, TestData.Pwd)
+    cy.get(TodoPage.Header).should("be.visible")
+    cy.get(TodoPage.ProjectTitle).should("be.visible").should("have.text",TestData.DefaultPrj)
+  })
 
-it('Verify Login', () => {
-Functions.LogIn(TestData.Validemail, TestData.Pwd)
-
-cy.get(TodoPage.Header).should("be.visible")
-cy.get(TodoPage.ProjectTitle).should("be.visible").should("have.text",TestData.DefaultPrj)
-})
-
-
-it('Verify Adding a project', () => {
-
-Functions.LogIn(TestData.Validemail, TestData.Pwd)
-let Donecount = 0
-cy.get(TodoPage.AddProject).first().click()
-cy.get(TodoPage.AddprjInput).type(TestData.NewPrj)
-cy.get(TodoPage.AddprjButton).click()
-cy.get(TodoPage.Newproject).last().click()
-cy.get(TodoPage.ProjectTitle).should("have.text",TestData.NewPrj)
-cy.get(TodoPage.noitems).should("have.text",TestData.Noitems)
-cy.get(TodoPage.donecount).should("have.text",Donecount)
-
-cy.get(TodoPage.AddItem).type(TestData.Newitem)
-cy.get(TodoPage.Itembutton).click()
-cy.get(TodoPage.noitems).should("not.be.visible")
-cy.get(TodoPage.ItemContent).should("have.text",TestData.Newitem)
-Donecount++
-
-cy.get(TodoPage.Donebox).click()
-cy.get(TodoPage.noitems).should("have.text",TestData.Noitems)
-cy.get(TodoPage.donecount).should("have.text",Donecount)
-
-})
+  it('Verify Adding a project', () => {
+    let Donecount = 0 //doneitem counter
+    cy.log('Log in to the system and add a project')
+    Functions.LogIn(TestData.Validemail, TestData.Pwd)
+    cy.get(TodoPage.AddProject).first().click()
+    cy.get(TodoPage.AddprjInput).type(TestData.NewPrj)
+    cy.get(TodoPage.AddprjButton).click()
+    cy.get(TodoPage.Newproject).last().click()
+    cy.get(TodoPage.ProjectTitle).should("have.text",TestData.NewPrj)
+    cy.get(TodoPage.noitems).should("have.text",TestData.Noitems)
+    cy.get(TodoPage.donecount).should("have.text",Donecount)
+    
+    cy.log('Add an item')
+    cy.get(TodoPage.AddItem).type(TestData.Newitem)
+    cy.get(TodoPage.Itembutton).click()
+    cy.get(TodoPage.noitems).should("not.be.visible")
+    cy.get(TodoPage.ItemContent).should("have.text",TestData.Newitem)
+    
+    cy.log('Check created item to done')
+    cy.get(TodoPage.Donebox).click()
+    Donecount++
+    cy.get(TodoPage.noitems).should("have.text",TestData.Noitems)
+    cy.get(TodoPage.donecount).should("have.text",Donecount)
+  })
 })
