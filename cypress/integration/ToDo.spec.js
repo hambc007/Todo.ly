ea2ee061151eef3b8ed25629db5bd7c7eb0e30e7
unknown <hamdibc0@gmail.com>
2021-07-25 17:24:54 +0200

    fix circleci build

--- cypress/integration/ToDo.spec.js
+++ cypress/integration/ToDo.spec.js
@@ -1,6 +1,6 @@
 const TodoPage = require("../PageObjects/TodoPage")
-const Login = require("../PageObjects/Login")
 const Signup = require("../PageObjects/Signup")
+const Login = require("../PageObjects/LogIn")
 const Functions = require("../support/Functions")
 const TestData = require("../support/TestData")
 
